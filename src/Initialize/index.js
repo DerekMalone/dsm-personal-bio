fde5b43d11a4a3ab5875284f9a62cb165a52f27f
DerekMalone <laxmalone@gmail.com>
2021-11-16 19:13:40 -0600

    saving

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
 import BioNavbar from '../components/Navbar';
 import Routes from '../routes/Routes';
+import { SignIn } from '../views';
 
 function Initialize() {
-  // const [user, setUser] = userState(null);
+  const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-
-  // }, [input])
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userObj = {
+          uid: authed.uid,
+          fullName: authed.displayName,
+          user: authed.email.split('@')[0],
+        };
+        setUser(userObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
 
   return (
     <>
+      <SignIn />
       <BioNavbar />
       <Routes />
     </>
