4929fa4377121888e99e554c180542a67e7eca76
DerekMalone <laxmalone@gmail.com>
2021-11-17 22:36:48 -0600

    saving progress

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,10 +1,23 @@
 import React from 'react';
+// import { useState } from 'react/cjs/react.development';
+// import { getRepoList } from '../helpers/projectsData';
+// , { getProject, useEffect }
 // import PropTypes from 'prop-types'
 
 export default function ProjectDetails() {
+  // const [projects, setProjects] = useState({});
+  // useEffect(() => {
+  //   // getProject().then(setRepos);
+  // }, []);
+
+  // (repoArray) => getProject(repoArray).then(setRepos)
+
+  // console.warn(repos);
+
   return (
     <div>
       <h1>Top 4 Projects Go Here</h1>
+      <div>{/* <h3></h3> */}</div>
     </div>
   );
 }
--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProjectDetails from '../components/ProjectDetails';
+import { getRepoList } from '../helpers/projectsData';
 
 export default function Projects() {
+  const [repos, setRepos] = useState({});
+  useEffect(() => {
+    getRepoList().then(setRepos);
+  }, []);
+
+  console.warn(repos);
+
   return (
     <div>
-      <ProjectDetails />
+      <ProjectDetails repos={repos} />
     </div>
   );
 }
