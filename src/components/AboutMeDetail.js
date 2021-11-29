a960b591bbec52177fe0a207a4a95fa4bc892021
DerekMalone <laxmalone@gmail.com>
2021-11-28 20:40:06 -0600

    simple style setup for all but tech Stacks

--- src/components/AboutMeDetail.js
+++ src/components/AboutMeDetail.js
@@ -1,22 +1,64 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex: wrap:
+  width: 100vw;
+  width: 95vw;
+  height: 100vh;
+  margin: 15px;
+
+  .name-style {
+    justify-content: center;
+    margin-left: 35vw;
+    margin-right: ;
+    color: #F2CC8F;
+  }
+
+  .about-style {
+    flex: wrap;
+    justify-content: space-around;
+    width: 90%
+  }
+
+  .about-font-style {
+    color: #81B29A;
+  }
+
+  .photo {
+    flex: wrap;
+    width: 100%;
+    height: 100%;
+  }
+
+`;
 
 export default function AboutMeDetail({ aboutMe }) {
   return (
-    <>
-      <h1>{aboutMe.name}</h1>
+    <Container>
       <div>
-        <p>{aboutMe.about}</p>
-        <hr />
-        <a href={aboutMe.photoHref}>
-          <img
-            src={aboutMe.photoUrl}
-            title={aboutMe.photoTitle}
-            alt={aboutMe.name}
-          />
-        </a>
+        <div className="name-style">
+          <h1>{aboutMe.name}</h1>
+        </div>
+        <div>
+          <div className="about-style">
+            <p className="about-font-style">{aboutMe.about}</p>
+          </div>
+        </div>
+        <div>
+          <a href={aboutMe.photoHref}>
+            <img
+              className="photo"
+              src={aboutMe.photoUrl}
+              title={aboutMe.photoTitle}
+              alt={aboutMe.name}
+            />
+          </a>
+        </div>
       </div>
-    </>
+    </Container>
   );
 }
 
--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,29 +1,77 @@
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 import React from 'react';
+
+const ContactCard = styled.div`
+  display: flex;
+  margin: 5vh 20vw;
+  border: solid 2px #e07a5f;
+  border-radius: 10px;
+  background-color: #81b29a;
+  width: 50vw;
+
+  .contents-style {
+    flex: wrap;
+    justify-content: space-between;
+  }
+
+  .contact-style {
+    margin: 5px 100%;
+    width: 100%;
+    color: #f2cc8f;
+  }
+
+  .email-style {
+    margin: 5px 85%;
+    flex: wrap;
+    width: 100%;
+  }
+
+  .phone-style {
+    margin: 5px 105%;
+    flex: wrap;
+    width: 100%;
+  }
+
+  .gitHub-style {
+    margin: 5px 90%;
+    flex: wrap;
+    width: 100%;
+  }
+
+  .linked-style {
+    margin: 5px 90%;
+    flex: wrap;
+    width: 100%;
+  }
+`;
 
 export default function ContactMe({ contact }) {
   return (
     <>
-      <div>
-        <h3>Contact Me</h3>
-        <div>
-          {contact.email} <i className="far fa-envelope" />
+      <ContactCard>
+        <div className="contents-style">
+          <div className="contact-style">
+            <h3>Contact Me</h3>
+          </div>
+          <div className="email-style">
+            {contact.email} <i className="far fa-envelope" />
+          </div>
+          <div className="phone-style">
+            {contact.phone} <i className="fas fa-phone" />
+          </div>
+          <div className="gitHub-style">
+            <a href={contact.gitHub}>
+              Derek Malone GitHub <i className="fab fa-github" />
+            </a>
+          </div>
+          <div className="linked-style">
+            <a href={contact.linkedIn}>
+              Derek Malone Linkedin <i className="fab fa-linkedin" />
+            </a>
+          </div>
         </div>
-        <div>
-          {contact.phone} <i className="fas fa-phone" />
-        </div>
-        <div>
-          <a href={contact.gitHub}>
-            Derek Malone GitHub <i className="fab fa-github" />
-          </a>
-        </div>
-        <div>
-          <a href={contact.linkedIn}>
-            Derek Malone Linkedin <i className="fab fa-linkedin" />
-          </a>
-        </div>
-      </div>
+      </ContactCard>
     </>
   );
 }
